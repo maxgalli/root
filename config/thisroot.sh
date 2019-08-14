9d724cf8077bb0302022878f0c14fd61288f2934
maxgalli <massimiliano.galli.95@gmail.com>
2019-08-14 10:00:41 +0200

    [Exp PyROOT] Build ROOT with multiple Python versions (2)
    Here we make the necessary changes to allow the user to choose a specific
    PyROOT build directory when source thisroot.sh.
    Source a specific Python version by calling:
    
    $ PYTHON_VERSION={version_number} source bin/thisroot.sh
    
    If PYTHON_VERSION is not provided, the default version (the one located in
    lib/python_default) is sourced.
    If the PYTHON_VERSION is not valid (i.e. the corrispondent build does not
    exist) a message is printed and no action is performed.

--- config/thisroot.sh
+++ config/thisroot.sh
@@ -24,14 +24,170 @@ drop_from_path()
                           -e "s;^${drop}\$;;g"`
 }
 
+drop_from_all_libs()
+{
+   if [ -n "${PATH}" ]; then
+      drop_from_path "$PATH" "${old_rootsys}/bin"
+      PATH=$newpath
+   fi
+   if [ -n "${LD_LIBRARY_PATH}" ]; then
+      drop_from_path "$LD_LIBRARY_PATH" "${old_rootsys}/lib"
+      LD_LIBRARY_PATH=$newpath
+      for pyroot_libs_dir in ${old_rootsys}/lib/python*
+      do
+         drop_from_path "$LD_LIBRARY_PATH" "$pyroot_libs_dir"
+         LD_LIBRARY_PATH=$newpath
+      done
+   fi
+   if [ -n "${DYLD_LIBRARY_PATH}" ]; then
+      drop_from_path "$DYLD_LIBRARY_PATH" "${old_rootsys}/lib"
+      DYLD_LIBRARY_PATH=$newpath
+      for pyroot_libs_dir in ${old_rootsys}/lib/python*
+      do
+         drop_from_path "$DYLD_LIBRARY_PATH" "$pyroot_libs_dir"
+         DYLD_LIBRARY_PATH=$newpath
+      done
+   fi
+   if [ -n "${SHLIB_PATH}" ]; then
+      drop_from_path "$SHLIB_PATH" "${old_rootsys}/lib"
+      SHLIB_PATH=$newpath
+      for pyroot_libs_dir in ${old_rootsys}/lib/python*
+      do
+         drop_from_path "$SHLIB_PATH" "$pyroot_libs_dir"
+         SHLIB_PATH=$newpath
+      done
+   fi
+   if [ -n "${LIBPATH}" ]; then
+      drop_from_path "$LIBPATH" "${old_rootsys}/lib"
+      LIBPATH=$newpath
+      for pyroot_libs_dir in ${old_rootsys}/lib/python*
+      do
+         drop_from_path "$LIBPATH" "$pyroot_libs_dir"
+         LIBPATH=$newpath
+      done
+   fi
+   if [ -n "${PYTHONPATH}" ]; then
+      drop_from_path "$PYTHONPATH" "${old_rootsys}/lib"
+      PYTHONPATH=$newpath
+      for pyroot_libs_dir in ${old_rootsys}/lib/python*
+      do
+         drop_from_path "$PYTHONPATH" "$pyroot_libs_dir"
+         PYTHONPATH=$newpath
+      done
+   fi
+   if [ -n "${MANPATH}" ]; then
+      drop_from_path "$MANPATH" "${old_rootsys}/man"
+      MANPATH=$newpath
+   fi
+   if [ -n "${CMAKE_PREFIX_PATH}" ]; then
+      drop_from_path "$CMAKE_PREFIX_PATH" "${old_rootsys}"
+      CMAKE_PREFIX_PATH=$newpath
+   fi
+   if [ -n "${JUPYTER_PATH}" ]; then
+      drop_from_path "$JUPYTER_PATH" "${old_rootsys}/etc/notebook"
+      JUPYTER_PATH=$newpath
+   fi
+   if [ -z "${MANPATH}" ]; then
+      # Grab the default man path before setting the path to avoid duplicates
+      if command -v manpath >/dev/null; then
+         default_manpath=`manpath`
+      elif command -v man >/dev/null; then
+         default_manpath=`man -w 2> /dev/null`
+      else
+         default_manpath=""
+      fi
+   fi
+}
+
+append_to_all_libs()
+{
+   # Assert that we got enough arguments
+   if test $# -ne 1 ; then
+      echo "drop_from_path: needs 1 argument"
+      return 1
+   fi
+
+   local version=$1
+
+if [ -z "${PATH}" ]; then
+  PATH=@bindir@; export PATH
+else
+  PATH=@bindir@:$PATH; export PATH
+fi
+
+if [ -z "${LD_LIBRARY_PATH}" ]; then
+  LD_LIBRARY_PATH=@libdir@:@libdir@/python${version}
+  export LD_LIBRARY_PATH       # Linux, ELF HP-UX
+else
+  LD_LIBRARY_PATH=@libdir@:@libdir@/python${version}:$LD_LIBRARY_PATH
+  export LD_LIBRARY_PATH
+fi
+
+if [ -z "${DYLD_LIBRARY_PATH}" ]; then
+  DYLD_LIBRARY_PATH=@libdir@:@libdir@/python${version}
+  export DYLD_LIBRARY_PATH       # Linux, ELF HP-UX
+else
+  DYLD_LIBRARY_PATH=@libdir@:@libdir@/python${version}:$DYLD_LIBRARY_PATH
+  export DYLD_LIBRARY_PATH
+fi
+
+if [ -z "${SHLIB_PATH}" ]; then
+  SHLIB_PATH=@libdir@:@libdir@/python${version}
+  export SHLIB_PATH       # Linux, ELF HP-UX
+else
+  SHLIB_PATH=@libdir@:@libdir@/python${version}:$SHLIBR_PATH
+  export SHLIB_PATH
+fi
+
+if [ -z "${LIBPATH}" ]; then
+  LIBPATH=@libdir@:@libdir@/python${version}
+  export LIBPATH       # Linux, ELF HP-UX
+else
+  LIBPATH=@libdir@:@libdir@/python${version}:$LIBPATH
+  export LIBPATH
+fi
+
+if [ -z "${PYTHONPATH}" ]; then
+  PYTHONPATH=@libdir@:@libdir@/python${version}
+  export PYTHONPATH       # Linux, ELF HP-UX
+else
+  PYTHONPATH=@libdir@:@libdir@/python${version}:$PYTHONPATH
+  export PYTHONPATH
+fi
+
+if [ -z "${MANPATH}" ]; then
+  MANPATH=@mandir@:${default_manpath}; export MANPATH
+else
+  MANPATH=@mandir@:$MANPATH; export MANPATH
+fi
+
+if [ -z "${CMAKE_PREFIX_PATH}" ]; then
+  CMAKE_PREFIX_PATH=$ROOTSYS; export CMAKE_PREFIX_PATH       # Linux, ELF HP-UX
+else
+  CMAKE_PREFIX_PATH=$ROOTSYS:$CMAKE_PREFIX_PATH; export CMAKE_PREFIX_PATH
+fi
+
+if [ -z "${JUPYTER_PATH}" ]; then
+  JUPYTER_PATH=$ROOTSYS/etc/notebook; export JUPYTER_PATH       # Linux, ELF HP-UX
+else
+  JUPYTER_PATH=$ROOTSYS/etc/notebook:$JUPYTER_PATH; export JUPYTER_PATH
+fi
+}
+
+
+### main ###
+
+
 if [ -n "${ROOTSYS}" ] ; then
    old_rootsys=${ROOTSYS}
 fi
+
 
 SOURCE=${BASH_ARGV[0]}
 if [ "x$SOURCE" = "x" ]; then
     SOURCE=${(%):-%N} # for zsh
 fi
+
 
 if [ "x${SOURCE}" = "x" ]; then
     if [ -f bin/thisroot.sh ]; then
@@ -49,110 +205,33 @@ else
     ROOTSYS=$(cd ${thisroot}/.. > /dev/null;pwd); export ROOTSYS
 fi
 
-if [ -n "${old_rootsys}" ] ; then
-   if [ -n "${PATH}" ]; then
-      drop_from_path "$PATH" "${old_rootsys}/bin"
-      PATH=$newpath
-   fi
-   if [ -n "${LD_LIBRARY_PATH}" ]; then
-      drop_from_path "$LD_LIBRARY_PATH" "${old_rootsys}/lib"
-      LD_LIBRARY_PATH=$newpath
-   fi
-   if [ -n "${DYLD_LIBRARY_PATH}" ]; then
-      drop_from_path "$DYLD_LIBRARY_PATH" "${old_rootsys}/lib"
-      DYLD_LIBRARY_PATH=$newpath
-   fi
-   if [ -n "${SHLIB_PATH}" ]; then
-      drop_from_path "$SHLIB_PATH" "${old_rootsys}/lib"
-      SHLIB_PATH=$newpath
-   fi
-   if [ -n "${LIBPATH}" ]; then
-      drop_from_path "$LIBPATH" "${old_rootsys}/lib"
-      LIBPATH=$newpath
-   fi
-   if [ -n "${PYTHONPATH}" ]; then
-      drop_from_path "$PYTHONPATH" "${old_rootsys}/lib"
-      PYTHONPATH=$newpath
-   fi
-   if [ -n "${MANPATH}" ]; then
-      drop_from_path "$MANPATH" "${old_rootsys}/man"
-      MANPATH=$newpath
-   fi
-   if [ -n "${CMAKE_PREFIX_PATH}" ]; then
-      drop_from_path "$CMAKE_PREFIX_PATH" "${old_rootsys}"
-      CMAKE_PREFIX_PATH=$newpath
-   fi
-   if [ -n "${JUPYTER_PATH}" ]; then
-      drop_from_path "$JUPYTER_PATH" "${old_rootsys}/etc/notebook"
-      JUPYTER_PATH=$newpath
-   fi
 
-fi
-
-if [ -z "${MANPATH}" ]; then
-   # Grab the default man path before setting the path to avoid duplicates
-   if command -v manpath >/dev/null; then
-      default_manpath=`manpath`
-   elif command -v man >/dev/null; then
-      default_manpath=`man -w 2> /dev/null`
+if [ -z "${PYTHON_VERSION}" ] ; then
+   if [ -n "${old_rootsys}" ] ; then
+       drop_from_all_libs
+   fi
+   append_to_all_libs "_default"
+else
+   if [ -d "@libdir@/python${PYTHON_VERSION}" ]; then
+      if [ -n "${old_rootsys}" ] ; then
+         drop_from_all_libs
+      fi
+   append_to_all_libs "${PYTHON_VERSION}"
    else
-      default_manpath=""
+      echo ERROR: build with Python version "${PYTHON_VERSION}" not found.
+      echo Available versions:
+      for version_path in @libdir@/python*
+      do
+         version_number=${version_path#@libdir@/python}
+         if [ ${version_number} = "_default" ]; then
+            :
+         else
+            echo ${version_number}
+         fi
+      done
    fi
 fi
 
-if [ -z "${PATH}" ]; then
-   PATH=@bindir@; export PATH
-else
-   PATH=@bindir@:$PATH; export PATH
-fi
-
-if [ -z "${LD_LIBRARY_PATH}" ]; then
-   LD_LIBRARY_PATH=@libdir@; export LD_LIBRARY_PATH       # Linux, ELF HP-UX
-else
-   LD_LIBRARY_PATH=@libdir@:$LD_LIBRARY_PATH; export LD_LIBRARY_PATH
-fi
-
-if [ -z "${DYLD_LIBRARY_PATH}" ]; then
-   DYLD_LIBRARY_PATH=@libdir@; export DYLD_LIBRARY_PATH   # Mac OS X
-else
-   DYLD_LIBRARY_PATH=@libdir@:$DYLD_LIBRARY_PATH; export DYLD_LIBRARY_PATH
-fi
-
-if [ -z "${SHLIB_PATH}" ]; then
-   SHLIB_PATH=@libdir@; export SHLIB_PATH                 # legacy HP-UX
-else
-   SHLIB_PATH=@libdir@:$SHLIB_PATH; export SHLIB_PATH
-fi
-
-if [ -z "${LIBPATH}" ]; then
-   LIBPATH=@libdir@; export LIBPATH                       # AIX
-else
-   LIBPATH=@libdir@:$LIBPATH; export LIBPATH
-fi
-
-if [ -z "${PYTHONPATH}" ]; then
-   PYTHONPATH=@libdir@; export PYTHONPATH
-else
-   PYTHONPATH=@libdir@:$PYTHONPATH; export PYTHONPATH
-fi
-
-if [ -z "${MANPATH}" ]; then
-   MANPATH=@mandir@:${default_manpath}; export MANPATH
-else
-   MANPATH=@mandir@:$MANPATH; export MANPATH
-fi
-
-if [ -z "${CMAKE_PREFIX_PATH}" ]; then
-   CMAKE_PREFIX_PATH=$ROOTSYS; export CMAKE_PREFIX_PATH       # Linux, ELF HP-UX
-else
-   CMAKE_PREFIX_PATH=$ROOTSYS:$CMAKE_PREFIX_PATH; export CMAKE_PREFIX_PATH
-fi
-
-if [ -z "${JUPYTER_PATH}" ]; then
-   JUPYTER_PATH=$ROOTSYS/etc/notebook; export JUPYTER_PATH       # Linux, ELF HP-UX
-else
-   JUPYTER_PATH=$ROOTSYS/etc/notebook:$JUPYTER_PATH; export JUPYTER_PATH
-fi
 
 # Prevent Cppyy from checking the PCH (and avoid warning)
 export CLING_STANDARD_PCH=none
@@ -164,3 +243,6 @@ fi
 unset old_rootsys
 unset thisroot
 unset -f drop_from_path
+unset -f drop_from_all_libs
+unset -f append_to_all_libs
+unset PYTHON_VERSION
