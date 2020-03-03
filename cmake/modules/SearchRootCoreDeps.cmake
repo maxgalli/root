5d004cdf2b0266871321d408a2cb1e63e1f849a8
Massimiliano Galli <massimiliano.galli@cern.ch>
2020-03-03 11:17:34 +0100

    [CMake][Exp PyROOT] Change default logic in the search for Python
    - Check if the PYTHON_EXECUTABLE deprecated variable was passed by the
    user; if so, check weather it points to Python 2 or 3 and set the
    appropriate Python{X}_EXECUTABLE variable
    - Look for Python3 and set the deprecated variables to the ones set
    automatically by find_package(Python3 ...)
    - Look for Python2 and set the deprecated variables to the ones set
    automatically by find_package(Python2 ...) ONLY IF PYTHON3 WASN'T FOUND

--- cmake/modules/SearchRootCoreDeps.cmake
+++ cmake/modules/SearchRootCoreDeps.cmake
@@ -16,25 +16,7 @@ endif()
 # Python is required by header and manpage generation
 
 if(CMAKE_VERSION VERSION_GREATER_EQUAL 3.14)
-  # Check whether old `find_package(PythonInterp)` variable was passed.
-  # If so, it will be passed to find_package(Python) below. Otherwise,
-  # check what `python` points to: Python 2 or 3:
-  if(NOT PYTHON_EXECUTABLE)
-    find_package(Python3 COMPONENTS Interpreter Development REQUIRED)
-    if(Python3_Development_FOUND)
-      # Re-run, now with NumPy, but not required:
-      find_package(Python3 COMPONENTS NumPy QUIET)
-      # Compat with find_package(PythonInterp), find_package(PythonLibs)
-      set(PYTHON_EXECUTABLE "${Python3_EXECUTABLE}")
-      set(PYTHON_INCLUDE_DIRS "${Python3_INCLUDE_DIRS}")
-      set(PYTHON_LIBRARIES "${Python3_LIBRARIES}")
-      set(PYTHON_VERSION_MAJOR "${Python3_VERSION_MAJOR}")
-      set(PYTHON_VERSION_MINOR "${Python3_VERSION_MINOR}")
-      set(PYTHON_VERSION_STRING "${Python3_VERSION_MAJOR}_${Python3_VERSION_MINOR}")
-      set(NUMPY_FOUND ${Python3_NumPy_FOUND})
-      set(NUMPY_INCLUDE_DIRS "${Python3_NumPy_INCLUDE_DIRS}")
-    endif()
-  endif()
+
   if(PYTHON_EXECUTABLE)
     execute_process(COMMAND ${PYTHON_EXECUTABLE} -c "import sys;print(sys.version_info[0])"
                     OUTPUT_VARIABLE PYTHON_PREFER_VERSION
@@ -43,15 +25,29 @@ if(CMAKE_VERSION VERSION_GREATER_EQUAL 3.14)
       message(WARNING "Unable to determine version of ${PYTHON_EXECUTABLE}: ${PYTHON_PREFER_VERSION_ERR}")
     endif()
     string(STRIP "${PYTHON_PREFER_VERSION}" PYTHON_PREFER_VERSION)
+    set(Python${PYTHON_PREFER_VERSION}_EXECUTABLE "${PYTHON_EXECUTABLE}")
   endif()
 
-  message(STATUS "Preferring Python version ${PYTHON_PREFER_VERSION}")
+  find_package(Python3 COMPONENTS Interpreter Development QUIET)
+  if(Python3_Development_FOUND)
+    # Re-run, now with NumPy, but not required:
+    find_package(Python3 COMPONENTS NumPy QUIET)
+    # Compat with find_package(PythonInterp), find_package(PythonLibs)
+    set(PYTHON_EXECUTABLE "${Python3_EXECUTABLE}")
+    set(PYTHON_INCLUDE_DIRS "${Python3_INCLUDE_DIRS}")
+    set(PYTHON_LIBRARIES "${Python3_LIBRARIES}")
+    set(PYTHON_VERSION_MAJOR "${Python3_VERSION_MAJOR}")
+    set(PYTHON_VERSION_MINOR "${Python3_VERSION_MINOR}")
+    set(PYTHON_VERSION_STRING "${Python3_VERSION_MAJOR}_${Python3_VERSION_MINOR}")
+    set(NUMPY_FOUND ${Python3_NumPy_FOUND})
+    set(NUMPY_INCLUDE_DIRS "${Python3_NumPy_INCLUDE_DIRS}")
+  endif()
 
-  if("${PYTHON_PREFER_VERSION}" MATCHES "2")
-    find_package(Python2 COMPONENTS Interpreter Development REQUIRED)
-    if(Python2_Development_FOUND)
-      # Re-run, now with NumPy, but not required:
-      find_package(Python2 COMPONENTS NumPy QUIET)
+  find_package(Python2 COMPONENTS Interpreter Development QUIET)
+  if(Python2_Development_FOUND)
+    # Re-run, now with NumPy, but not required:
+    find_package(Python2 COMPONENTS NumPy QUIET)
+    if(NOT Python3_Development_FOUND)
       # Compat with find_package(PythonInterp), find_package(PythonLibs)
       set(PYTHON_EXECUTABLE "${Python2_EXECUTABLE}")
       set(PYTHON_INCLUDE_DIRS "${Python2_INCLUDE_DIRS}")
@@ -61,41 +57,7 @@ if(CMAKE_VERSION VERSION_GREATER_EQUAL 3.14)
       set(PYTHON_VERSION_STRING "${Python2_VERSION_MAJOR}_${Python2_VERSION_MINOR}")
       set(NUMPY_FOUND ${Python2_NumPy_FOUND})
       set(NUMPY_INCLUDE_DIRS "${Python2_NumPy_INCLUDE_DIRS}")
-      find_package(Python3 COMPONENTS Interpreter Development REQUIRED)
-      if(Python3_Development_FOUND)
-        # Re-run, now with NumPy, but not required:
-        find_package(Python3 COMPONENTS NumPy QUIET)
-        set(OTHER_PYTHON_EXECUTABLE "${Python3_EXECUTABLE}")
-        set(OTHER_PYTHON_INCLUDE_DIRS "${Python3_INCLUDE_DIRS}")
-        set(OTHER_PYTHON_LIBRARIES "${Python3_LIBRARIES}")
-        set(OTHER_PYTHON_VERSION_MAJOR "${Python3_VERSION_MAJOR}")
-        set(OTHER_PYTHON_VERSION_MINOR "${Python3_VERSION_MINOR}")
-        set(OTHER_PYTHON_VERSION_STRING "${Python3_VERSION_MAJOR}_${Python3_VERSION_MINOR}")
-        set(OTHER_NUMPY_FOUND ${Python3_NumPy_FOUND})
-        set(OTHER_NUMPY_INCLUDE_DIRS "${Python3_NumPy_INCLUDE_DIRS}")
-      endif()
-    endif()
-  elseif("${PYTHON_PREFER_VERSION}" MATCHES "3")
-    if(NOT Python3_EXECUTABLE)
-      find_package(Python3 COMPONENTS Interpreter Development REQUIRED)
-      if(Python3_Development_FOUND)
-        # Re-run, now with NumPy, but not required:
-        find_package(Python3 COMPONENTS NumPy QUIET)
-        # Compat with find_package(PythonInterp), find_package(PythonLibs)
-        set(PYTHON_EXECUTABLE "${Python3_EXECUTABLE}")
-        set(PYTHON_INCLUDE_DIRS "${Python3_INCLUDE_DIRS}")
-        set(PYTHON_LIBRARIES "${Python3_LIBRARIES}")
-        set(PYTHON_VERSION_MAJOR "${Python3_VERSION_MAJOR}")
-        set(PYTHON_VERSION_MINOR "${Python3_VERSION_MINOR}")
-        set(PYTHON_VERSION_STRING "${Python3_VERSION_MAJOR}_${Python3_VERSION_MINOR}")
-        set(NUMPY_FOUND ${Python3_NumPy_FOUND})
-        set(NUMPY_INCLUDE_DIRS "${Python3_NumPy_INCLUDE_DIRS}")
-      endif()
-    endif()
-    find_package(Python2 COMPONENTS Interpreter Development REQUIRED)
-    if(Python2_Development_FOUND)
-      # Re-run, now with NumPy, but not required:
-      find_package(Python2 COMPONENTS NumPy QUIET)
+    else()
       set(OTHER_PYTHON_EXECUTABLE "${Python2_EXECUTABLE}")
       set(OTHER_PYTHON_INCLUDE_DIRS "${Python2_INCLUDE_DIRS}")
       set(OTHER_PYTHON_LIBRARIES "${Python2_LIBRARIES}")
@@ -106,7 +68,13 @@ if(CMAKE_VERSION VERSION_GREATER_EQUAL 3.14)
       set(OTHER_NUMPY_INCLUDE_DIRS "${Python2_NumPy_INCLUDE_DIRS}")
     endif()
   endif()
+
+  if(NOT Python3_Development_FOUND AND NOT Python2_Development_FOUND)
+    message(FATAL_ERROR "No Python 2 or 3 were found")
+  endif()
+
 else()
+
   find_package(PythonInterp ${python_version} REQUIRED)
   find_package(PythonLibs ${python_version} REQUIRED)
 
@@ -119,6 +87,7 @@ else()
   find_package(NumPy QUIET)
 
   set(PYTHON_VERSION_STRING "${PYTHON_VERSION_MAJOR}_${PYTHON_VERSION_MINOR}")
+
 endif()
 
 # create lists of Python 2 and 3 useful variables
